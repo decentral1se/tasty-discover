9e29964c76c5a522dba6464a95c8571b7c48957b
Luke Murphy <lukewm@riseup.net>
2016-10-24 13:51:29 +0200

    Make sure `tasty-discover` is findings the tests.
    Closes #38.

---
+++ test/RunnerTest.hs
@@ -0,0 +1,20 @@
+-- Unit tests to assure `tasty-discover` is discovering tests.
+
+module RunnerTest where
+
+import Test.Tasty.Discover (Assertion, (@?), defaultConfig, getListOfTests)
+
+case_unitTestsDiscovered :: Assertion
+case_unitTestsDiscovered = do
+  unitTests <- getListOfTests "test" defaultConfig
+  (return $ null unitTests :: IO Bool) @? "Couldn't find any unit tests."
+
+case_integrationTestsDiscovered :: Assertion
+case_integrationTestsDiscovered = do
+  integrationTests <- getListOfTests "integration-test/" defaultConfig
+  (return $ null integrationTests :: IO Bool) @? "Couldn't find any integration tests."
+
+case_exampleTestsDiscovered :: Assertion
+case_exampleTestsDiscovered = do
+  exampleTests <- getListOfTests "example/" defaultConfig
+  (return $ null exampleTests :: IO Bool) @? "Couldn't find any example tests."
