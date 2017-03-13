ff2ff82b73a4aff89a3822ac689d779d78a0ccf2
Luke Murphy <lukewm@riseup.net>
2017-03-13 18:22:34 +0000

    Add example project.

---
+++ example-usage/test/AllTheFolders/AnotherNestTest.hs
@@ -0,0 +1,4 @@
+module AllTheFolders.AnotherNestTest where
+
+prop_nineIsNine :: Bool
+prop_nineIsNine = 9 == (9 :: Integer)
---
+++ example-usage/test/Thing/AnotherThing/NestedTest.hs
@@ -0,0 +1,4 @@
+module Thing.AnotherThing.NestedTest where
+
+prop_twoIsTwo :: Bool
+prop_twoIsTwo = 2 == (2 :: Integer)
