e43e916871ef28aec5f3cc4c222bfb44cc19c429
Luke Murphy <lukewm@riseup.net>
2017-03-14 09:48:29 +0000

    Remove example project. Unit test does the job.
    Also, I improved the readme so, hopefully it just isn't
    necessary.

--- example-usage/test/BarTest.hs
+++
@@ -1,8 +0,0 @@
-module BarTest where
-
-import Test.Tasty.Hspec (hspec, describe, it, shouldBe)
-
-case_headIsWorking = hspec $
-  describe "Does Prelude.head still work?" $
-    it "returns the first element of a list" $
-      head [23..] `shouldBe` (23 :: Int)
--- example-usage/test/Thing/AnotherThing/NestedTest.hs
+++
@@ -1,4 +0,0 @@
-module Thing.AnotherThing.NestedTest where
-
-prop_twoIsTwo :: Bool
-prop_twoIsTwo = 2 == (2 :: Integer)
