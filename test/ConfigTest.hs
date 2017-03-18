5cc47e4bd911a3b98fbf0f32e21310a325b6b14b
Luke Murphy <lukewm@riseup.net>
2017-03-18 10:25:15 +0000

    Use default config record syntax.

--- test/ConfigTest.hs
+++ test/ConfigTest.hs
@@ -8,26 +8,21 @@ import Test.Tasty.Generator (mkTest)
 
 case_noModuleSuffixEmptyList :: IO ()
 case_noModuleSuffixEmptyList = do
-  actual <- findTests "test/SubMod/" config
+  actual <- findTests "test/SubMod/" (defaultConfig { moduleSuffix = Just "DoesntExist"})
   actual @?= []
-  where
-    config = Config (Just "DoesntExist") Nothing [] [] False False
 
 case_differentGeneratedModule :: Assertion
-case_differentGeneratedModule = assertBool "Specified module is used" test
-  where test = "FunkyModuleName" `isInfixOf` generatedModule
-        generatedModule = generateTestDriver "FunkyModuleName" [] "test/" []
+case_differentGeneratedModule = assertBool "" ("FunkyModuleName" `isInfixOf` generatedModule)
+  where generatedModule = generateTestDriver "FunkyModuleName" [] "test/" []
 
 case_ignoreAModule :: IO ()
 case_ignoreAModule = do
-  actual <- findTests "test/SubMod/" config
+  actual <- findTests "test/SubMod/" (defaultConfig { ignoredModules = ["PropTest"] })
   actual @?= []
-  where
-    config = Config Nothing Nothing ["PropTest"] [] False False
 
 case_noModuleSuffix :: IO ()
 case_noModuleSuffix  = do
-  actual1 <- findTests "test/SubMod/" config1
+  actual1 <- findTests "test/SubMod/" defaultConfig
   actual1 @?= [mkTest "PropTest" "prop_addition_is_associative"]
 
   actual2 <- findTests "test/SubMod/" config2
