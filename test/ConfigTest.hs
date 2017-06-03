686f482d54152e20818f80c325dcf0b356ea5e37
Mike Craig <mike@mkscrg.net>
2017-06-03 04:00:00 -0700

    add --tree-display flag and failing unit test

--- test/ConfigTest.hs
+++ test/ConfigTest.hs
@@ -2,7 +2,7 @@ module ConfigTest where
 
 import           Data.List            (isInfixOf)
 import           Test.Tasty.Config
-import           Test.Tasty.Discover  (findTests, generateTestDriver)
+import           Test.Tasty.Discover  (findTests, generateTestDriver, showTests)
 import           Test.Tasty.Generator (mkTest)
 import           Test.Tasty.HUnit
 
@@ -27,6 +27,7 @@ unit_noModuleSuffix  = do
 
   actual2 <- findTests "test/SubMod/" (defaultConfig { noModuleSuffix = True })
   let expected = [ mkTest "FooBaz" "prop_additionCommutative"
+                 , mkTest "FooBaz" "prop_multiplationDistributiveOverAddition"
                  , mkTest "PropTest" "prop_additionAssociative" ]
   assertBool "" $ all (`elem` expected) actual2
 
@@ -34,3 +35,17 @@ unit_noModuleSuffixRecurseDirs :: IO ()
 unit_noModuleSuffixRecurseDirs = do
   tests <- findTests "test/" (defaultConfig { noModuleSuffix = True })
   assertBool "" $ elem (mkTest "SubMod/FooBaz" "prop_additionCommutative") tests
+
+unit_noModuleTreeDefault :: IO ()
+unit_noModuleTreeDefault = do
+  tests <- findTests "test/SubMod/" (defaultConfig { noModuleSuffix = True })
+  let testNumVars = map (('t' :) . show) [(0::Int)..]
+      trees = showTests tests testNumVars
+  length trees @?= 3
+
+unit_moduleTree :: IO ()
+unit_moduleTree = do
+  tests <- findTests "test/SubMod/" (defaultConfig { noModuleSuffix = True })
+  let testNumVars = map (('t' :) . show) [(0::Int)..]
+      trees = showTests tests testNumVars
+  length trees @?= 2
